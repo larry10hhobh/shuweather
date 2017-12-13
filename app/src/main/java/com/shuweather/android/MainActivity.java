dcb41102ad0b3de8764ddf5a7189e258fc7f6a43
Larry <larry10hhobh@gmail.com>
2017-12-14 01:22:04 +0800

    遍历省市县1.0

--- app/src/main/java/com/shuweather/android/MainActivity.java
+++ app/src/main/java/com/shuweather/android/MainActivity.java
@@ -1,4 +1,4 @@
-package com.shuweather.android;
+ package com.shuweather.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
